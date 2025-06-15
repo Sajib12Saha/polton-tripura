56ac85b212ebb624bee8084ec82bbb5acc57fda6
Sajib12Saha <sahasojib0155@gmail.com>
2025-06-15 10:56:28 +0600

    fix postcss issues

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,5 @@
-
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: [
-    tailwindcss("./tailwind.config.mjs"),
-    autoprefixer,
-  ],
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;
