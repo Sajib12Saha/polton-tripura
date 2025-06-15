1e1ecdcbfc8e46d77a650b7d59fbe4bb44bb6605
Sajib12Saha <sahasojib0155@gmail.com>
2025-06-15 16:05:07 +0600

    fix parmas in blog

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -3,37 +3,36 @@ import { BlogCard } from "@/components/blog-card";
 import { BlogType } from "@/types/type";
 import Link from "next/link";
 
-
 interface BlogPageProps {
-  searchParams?: {
-    page?: string;
-  };
+  searchParams?: Record<string, string | string[] | undefined>;
 }
 
+const BlogPage = async ({ searchParams }: BlogPageProps) => {
+  const pageStr = searchParams?.page;
+  const currentPage = Number(Array.isArray(pageStr) ? pageStr[0] : pageStr) || 1;
 
-const BlogPage = async({searchParams}:BlogPageProps) =>{
-      const currentPage = Number( searchParams?.page || 1);
-        const {data:blogs, totalPages} = await getBlogs(currentPage)
-    return (
-        <div className="mt-18 space-y-8">
-            <div className="flex flex-col items-center">
-             <p className="text-primary font-semibold text-lg md:text-xl">Visit my blog and keep your feedback</p>
+  const { data: blogs, totalPages } = await getBlogs(currentPage);
+
+  return (
+    <div className="mt-18 space-y-8">
+      <div className="flex flex-col items-center">
+        <p className="text-primary font-semibold text-lg md:text-xl">
+          Visit my blog and keep your feedback
+        </p>
         <h2 className="text-5xl md:text-6xl font-bold dark:text-gray-300">My Blog</h2>
-            </div>
+      </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {blogs.map((blog:BlogType, index:number)=>(
-        <BlogCard key={index} blog={blog}/>
-            ))}
-            
-         </div>
-               <div className="flex justify-center items-center gap-4 pt-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {blogs.map((blog: BlogType, index: number) => (
+          <BlogCard key={index} blog={blog} />
+        ))}
+      </div>
+
+      <div className="flex justify-center items-center gap-4 pt-10">
         <Link
           href={`?page=${currentPage - 1}`}
           className={`px-4 py-2 shadow-[3px_3px_3px_rgba(0,0,0,0.25),-1px_-1px_4px_rgba(255,255,255,0.8)] dark:shadow-[3px_3px_3px_rgba(0,0,0,0.25),-1px_-1px_4px_rgba(255,255,255,0.16)]  rounded ${
-            currentPage === 1
-              ? "pointer-events-none opacity-50"
-              : "hover:bg-primary  transition"
+            currentPage === 1 ? "pointer-events-none opacity-50" : "hover:bg-primary  transition"
           }`}
         >
           Previous
@@ -46,19 +45,14 @@ const BlogPage = async({searchParams}:BlogPageProps) =>{
         <Link
           href={`?page=${currentPage + 1}`}
           className={`px-4 py-2 shadow-[3px_3px_3px_rgba(0,0,0,0.25),-1px_-1px_4px_rgba(255,255,255,0.8)] dark:shadow-[3px_3px_3px_rgba(0,0,0,0.25),-1px_-1px_4px_rgba(255,255,255,0.16)]   rounded ${
-            currentPage >= totalPages
-              ? "pointer-events-none opacity-50"
-              : "hover:bg-primary  transition"
+            currentPage >= totalPages ? "pointer-events-none opacity-50" : "hover:bg-primary  transition"
           }`}
-          
         >
-      
-
           Next
         </Link>
       </div>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default BlogPage;
