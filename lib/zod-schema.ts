1067f8fc404fac731ea8fb4d807eaebc645726ac
Sajib12Saha <145220509+Sajib12Saha@users.noreply.github.com>
2025-06-19 12:16:33 +0600

    Update zod-schema.ts

--- lib/zod-schema.ts
+++ lib/zod-schema.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 
 export  const contactSchema = z.object({
         name: z.string().min(2, "Name must be at least 2 characters long"),
-        phone: z.string().regex(/^\+?[0-9]\d{1,14}$/, "Invalid phone number").optional(),
         email: z.string().email("Invalid email address"),
         subject:z.string().optional(),
         message: z.string().min(3, "Message must be at least 3 characters long"),
